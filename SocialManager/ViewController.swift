a4bf5965627a60e4095ec4f0a35859d50d37b2f3
Aynur Galiev <aynur.galiev@flatstack.com>
2015-09-23 12:22:25 +0300

    Initial Commit

---
+++ SocialManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialManager
+//
+//  Created by Aynur Galiev on 23.09.15.
+//  Copyright (c) 2015 Flatstack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
